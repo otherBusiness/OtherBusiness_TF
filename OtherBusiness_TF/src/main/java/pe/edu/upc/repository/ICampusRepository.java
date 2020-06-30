7838a36b798310f9d67426cbba6fbcf0cdd521f3
joao <pc@LAPTOP-QHRJIKU3>
2020-06-29 23:31:31 -0500

    top 5 campus mas usados

--- OtherBusiness_TF/src/main/java/pe/edu/upc/repository/ICampusRepository.java
+++ OtherBusiness_TF/src/main/java/pe/edu/upc/repository/ICampusRepository.java
@@ -16,7 +16,7 @@ public interface ICampusRepository extends JpaRepository<Campus, Integer>{
 	public int searchCampus(@Param("nameCampus") String nombre);
 	
 	
-	@Query(value = "SELECT c.name_campus, count(pr.id_project) from public.investors_project i join invesment_details ide on ide.id_investor_project=i.id_investor_project join projects pr on ide.id_project= pr.id_project join campus c on c.id_campus= c.id_campus group by c.name_campus order by count(c.name_campus) desc limit 1", nativeQuery = true) 
+	@Query(value = "SELECT c.name_campus, count(pr.id_project) from public.investors_project i join invesment_details ide on ide.id_investor_project=i.id_investor_project join projects pr on ide.id_project= pr.id_project join campus c on c.id_campus= c.id_campus group by c.name_campus order by count(c.name_campus) desc limit 5", nativeQuery = true) 
 	//count(pr.id_product) from public.importation i join import_details ide on  ide.id_importation = i.id_importation join products pr on ide.id_product = pr.id_product join brands c on c.id_brand= pr.id_brand group by c.brand_name ORDER BY COUNT(c.brand_name) DESC limit 1", nativeQuery = true)
 	public List<String[]> masCampus();
 }
